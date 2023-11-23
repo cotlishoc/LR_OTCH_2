63c5a938fac72fc6c5790c04e62c2b3290dd935f
cotlishoc <131800742+cotlishoc@users.noreply.github.com>
2023-11-23 14:54:35 +0300

    Add files via upload

---
+++ z1.pas
@@ -0,0 +1,19 @@
+﻿const m=5;
+var a: array [1..m] of integer;
+i: integer;
+n: boolean;
+begin
+  writeln('Введите массив: ');
+  for i:=1 to m do
+    read(a[i]);
+  for i:=1 to m-1 do begin
+    if a[i]>=a[i+1] then begin
+      n:= false;
+      break;
+      end
+      else
+        n:= true;
+  end;
+  if n then writeln ('Массив является возрастающим')
+  else writeln('Массив не является возрастающим')
+end.  
---
+++ z2.pas
@@ -0,0 +1,29 @@
+﻿var
+  s: string;
+  i, dl, j, z: integer;
+  n: Boolean;
+begin
+  writeln('Введите строку');
+  readln(s);
+  z:=0;
+  dl:=length(s);
+  for i:=0 to dl-3 do
+  begin
+    j:=0;
+    n:=false;
+    while j<dl-3 do
+    begin
+      if (i+1=j+1) and (i+2=j+2) and (i+3=j+3) then //проверяет первую комбинацию
+        j:=j+1;  //для поиска других комбинаций
+      if (s[dl-2]=s[i+1]) and (s[dl-1]=s[i+2]) 
+            and (s[dl]=s[i+3]) and (i+1<>dl-2) and (i+2<>dl-1) and (i+3<>dl) then  //проверка уникальности комбинаций
+        n:=true;
+      if (s[i+1]=s[j+1]) and (s[i+2]=s[j+2]) and (s[i+3]=s[j+3]) then
+        n:=true;
+      j:=j+1;
+    end;
+    if n=false then 
+      z:=z+1;
+  end;
+  write('Количество уникальных подстрок из трёх символов: ', z)
+end.
